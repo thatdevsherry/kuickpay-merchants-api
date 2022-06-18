063ec6bcf06c3a25a933cfca1d410efb21e30daf
Shehriyar Qureshi <thatdevsherry@gmail.com>
2022-06-18 20:44:57 +0500

    feat(): remove app.controller and app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BillInquiryModule } from './modules/bill-inquiry/bill-inquiry.module';
 import { BillPaymentModule } from './modules/bill-payment/bill-payment.module';
 
 @Module({
   imports: [BillInquiryModule, BillPaymentModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
